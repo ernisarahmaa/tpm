8e86942972c3627a0a5e5282fbbccf2579a3127b
Dinda Aisyah Novitasari <100357324+dindanvtsr@users.noreply.github.com>
2023-03-07 02:34:25 +0700

    Update : UI Login Page

---
+++ sizes.dart
@@ -0,0 +1,4 @@
+//App default Sizing
+const tDefaultSize = 30.0;
+const tButtonHeight = 15.0;
+const tFormHeight = 20.0;
---
+++ text.dart
@@ -0,0 +1,16 @@
+// Login Page Text
+const String tNext = "Next";
+const String tLogin = "Login";
+const String tUsername = "Username";
+const String tPassword = "Password";
+const String tLoginTitle = "Welcome Back,";
+const String tLoginSubTitle = "You've been missed";
+
+// Landing Page Text
+const String tNama1 = "Ernisa Rahma Wahyuni";
+const String tNama2 = "Dinda Aisyah Novitasari";
+const String tNIM1 = "123200003";
+const String tNIM2 = "123200006";
+
+//Button
+const String tCalculate = "Calculate";
